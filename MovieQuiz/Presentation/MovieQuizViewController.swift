8b90440b1cfd049f4854296415d9e039666fc112
Elliah <sz281214.in@gmail.com>
2024-04-09 13:53:26 +0300

    Sprint 04 Fix

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,26 +1,120 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
-    // MARK: - Lifecycle
-    // Оутлеты
+    
+    // MARK: - IBOutlet
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var counterLabel: UILabel!
-    // добавил оутлеты кнопок для реализации логики, описанной ниже
     @IBOutlet private var noButton: UIButton!
     @IBOutlet private var yesButton: UIButton!
     
-    // Переменные
+    // MARK: - Private Properties
     private var currentQuestionIndex = 0 // текущей индекс вопроса в массиве моковых данных
     private var correctAnswers = 0 // счетчик правильных ответов
     
+    // MARK: - Public methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.layer.cornerRadius = 20 // скургление углов картинки
         show(quiz: convert(model: questions.first ?? QuizQuestion(image: "", text: "", correctAnswer: true)))
     }
-    // Экшены
-    @IBAction private func noButtonClicker(_ sender: Any) {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    // MARK: - Private methods
+    
+    // конвертируем моковые данные во вью модель
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let converted: QuizStepViewModel
+        if let value = UIImage(named: model.image) {
+            converted = QuizStepViewModel(image: value,
+                                          question: model.text,
+                                          questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+        } else {
+            converted = QuizStepViewModel(image: UIImage(),
+                                          question: model.text,
+                                          questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+        }
+       return converted
+    }
+    
+    // окрашиваем рамку 
+    private func showAnswerResult(isCorrect: Bool) {
+        lockButtons() // лочим нажатия на кнопки, когда ответ был получен
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.cornerRadius = 20
+        if isCorrect {
+            imageView.layer.borderColor = UIColor.ypGreen.cgColor
+            correctAnswers += 1
+            let generator = UINotificationFeedbackGenerator() // добавил taptic отклик
+                generator.notificationOccurred(.success)
+        } else {
+            imageView.layer.borderColor = UIColor.ypRed.cgColor
+            let generator = UINotificationFeedbackGenerator() // добавил taptic отклик
+                generator.notificationOccurred(.error)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionOrResult()
+            self.imageView.layer.borderWidth = 0
+            self.unlockButtons() // разрешаем нажатия на кнопки когда появляется следующий вопрос
+        }
+
+    }
+    
+    // показываем вопрос
+    private func show(quiz step: QuizStepViewModel) {
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
+    }
+    
+    private func lockButtons() {
+        noButton.isEnabled = false
+        yesButton.isEnabled = false
+    }
+    private func unlockButtons() {
+        noButton.isEnabled = true
+        yesButton.isEnabled = true
+    }
+    
+    private func showNextQuestionOrResult() {
+        if currentQuestionIndex == questions.count - 1 {
+            show(quiz: QuizResultsViewModel(title: "Раунд окончен", 
+                                            text: "Ваш результат: \(correctAnswers)/10",
+                                            buttonText: "Сыграть еще раз"))
+        } else {
+            currentQuestionIndex += 1
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(quiz: viewModel)
+        }
+    }
+    
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(title: result.title, message: result.text, preferredStyle: .alert)
         
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [self] _ in
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            
+            show(quiz: self.convert(model: questions.first ?? QuizQuestion(image: "", text: "", correctAnswer: true)))
+            
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - IBAction
+    
+    @IBAction private func noButtonClicker(_ sender: Any) {
         let currentQuestion = questions[currentQuestionIndex]
         showAnswerResult(isCorrect: currentQuestion.correctAnswer == false)
     }
@@ -30,6 +124,8 @@ final class MovieQuizViewController: UIViewController {
         showAnswerResult(isCorrect: currentQuestion.correctAnswer == true)
     }
     
+    // MARK: - Types
+    
     // структура моковых данных
     private struct QuizQuestion {
         let image: String // совпадает ли название фильма с названием картинки афиши
@@ -37,7 +133,6 @@ final class MovieQuizViewController: UIViewController {
         let correctAnswer: Bool // правильный ответ на вопрос
     }
     
-    // MARK: - view models
     // вью модель экрана
     private struct QuizStepViewModel {
       let image: UIImage // картинка с афишей
@@ -52,9 +147,10 @@ final class MovieQuizViewController: UIViewController {
         let buttonText: String
     }
     
-    // массив с моковыми данными
+    // MARK: - Constants
+    
     private let questions: [QuizQuestion] = [
-        QuizQuestion(image: "The Godfather", 
+        QuizQuestion(image: "The Godfather",
                      text: "Рейтинг этого фильма больше чем 6?",
                      correctAnswer: true),
         QuizQuestion(image: "The Dark Knight",
@@ -85,86 +181,17 @@ final class MovieQuizViewController: UIViewController {
                      text: "Рейтинг этого фильма больше чем 6?",
                      correctAnswer: false)
     ]
-    
-    // MARK: - functions
-    
-    // конвертируем моковые данные во вью модель
-    private func convert(model: QuizQuestion) -> QuizStepViewModel {
-        let converted: QuizStepViewModel
-        if let value = UIImage(named: model.image) {
-            converted = QuizStepViewModel(image: value, question: model.text, questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
-        } else {
-            converted = QuizStepViewModel(image: UIImage(), question: model.text, questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
-        }
-       return converted
-    }
-    
-    // окрашиваем рамку 
-    private func showAnswerResult(isCorrect: Bool) {
-        lockButtons() // лочим нажатия на кнопки, когда ответ был получен
-        imageView.layer.masksToBounds = true
-        imageView.layer.borderWidth = 8
-        imageView.layer.cornerRadius = 20
-        if isCorrect {
-            imageView.layer.borderColor = UIColor.ypGreen.cgColor
-            correctAnswers += 1
-        } else {
-            imageView.layer.borderColor = UIColor.ypRed.cgColor
-        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.showNextQuestionOrResult()
-            self.unlockButtons() // разрешаем нажатия на кнопки когда появляется следующий вопрос
-        }
-
-    }
-    
-    // показываем вопрос
-    private func show(quiz step: QuizStepViewModel) {
-        imageView.image = step.image
-        textLabel.text = step.question
-        counterLabel.text = step.questionNumber
-    }
-    // здесь добавляем функции которые разрешают или запрещают нажатия на кнопки
-    // это сделано для того, чтобы пользователь не мог пропускать вопросы, потому что следующий вопрос появляется через секунду после предыдущего, а кнопки активны всегда
-    // из-за этого бывает сбиваются результаты и выглядит все не очень красиво :)
-    private func lockButtons() {
-        noButton.isEnabled = false
-        yesButton.isEnabled = false
-    }
-    private func unlockButtons() {
-        noButton.isEnabled = true
-        yesButton.isEnabled = true
-    }
-    
-    private func showNextQuestionOrResult() {
-        if currentQuestionIndex == questions.count - 1 {
-            show(quiz: QuizResultsViewModel(title: "Раунд окончен", text: "Ваш результат: \(correctAnswers)/10", buttonText: "Сыграть еще раз"))
-        } else {
-            currentQuestionIndex += 1
-            let nextQuestion = questions[currentQuestionIndex]
-            let viewModel = convert(model: nextQuestion)
-            
-            show(quiz: viewModel)
-        }
-    }
-    
-    private func show(quiz result: QuizResultsViewModel) {
-        let alert = UIAlertController(title: result.title, message: result.text, preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: result.buttonText, style: .default) { [self] _ in
-            self.currentQuestionIndex = 0
-            self.correctAnswers = 0
-            
-            imageView.layer.borderWidth = 0 // при перезапуске пропадает рамка (вроде нигде в ТЗ об этом не было сказано, но как будто логично было это сделать пусть и таким кривым способом)
-            show(quiz: self.convert(model: questions.first ?? QuizQuestion(image: "", text: "", correctAnswer: true)))
-            
-        }
-        
-        alert.addAction(action)
-        
-        self.present(alert, animated: true, completion: nil)
-    }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 /*
